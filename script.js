ae92e934070f5caf723e94c49dbecdc80878dc80
petrspada-hub <249244861+petrspada-hub@users.noreply.github.com>
2026-03-18 09:15:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -449,6 +449,8 @@ html, body, canvas, #game, .hitbox { -webkit-tap-highlight-color: rgba(0,0,0,0) 
             updateImageForDifficulty();
             setMode(mode);
             reset(true);
+            running = true;
+            requestAnimationFrame(loop);
             return;
         }
         const rightWidth = 180, topHeight = 40;
